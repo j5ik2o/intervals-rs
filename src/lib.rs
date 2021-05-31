e4ad41a747cf107b68eecbdf813b58c8b972e5e1
Junichi Kato <j5ik2o@gmail.com>
2021-06-01 01:52:57 +0900

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod errors;
 mod interval;
 mod interval_limit;
 mod limit_value;
+mod interval_seq;
 
 #[cfg(test)]
 mod interval_limit_test;
@@ -10,10 +11,11 @@ mod interval_limit_test;
 mod interval_test;
 #[cfg(test)]
 mod limit_value_test;
-
-mod interval_seq;
+#[cfg(test)]
+mod interval_seq_test;
 
 pub use crate::errors::Error;
 pub use crate::limit_value::LimitValue;
 pub use crate::interval_limit::IntervalLimit;
 pub use crate::interval::Interval;
+pub use crate::interval_seq::IntervalSeq;
